5e96193ee5be5019806948060446ee38234ffa91
Kumod De Silva <116749725+KumodN@users.noreply.github.com>
2025-03-19 09:39:55 +0530

    added new section and recommendation section

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Banner from './Banner'
 import TopSellers from './TopSellers'
+import Recommend from './Recommend'
+import News from './News'
 
 const Home = () => {
   return (
     <>
         <Banner/>
         <TopSellers/>
+        <Recommend/>
+        <News/>
     </>
   )
 }
---
+++ frontend/src/pages/home/Recommend.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react'
+import GlassCard from "../glasses/GlassCard";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import required modules
+import { Navigation, Pagination } from "swiper/modules";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import 'swiper/css/navigation';
+
+const Recommend = () => {
+    const [glasses, setGlasses] = useState([]);
+    
+      useEffect(() => {
+        fetch("glasses.json")
+          .then((res) => res.json())
+          .then((data) => setGlasses(data));
+      }, []);
+  return (
+    <div className='py-16'>
+        <h2 className="text-3xl font-semibold mb-6">Recommended for you</h2>
+
+        <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+          navigation={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1180: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          }
+        }}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+
+{
+      glasses.length>0 && glasses.slice(8,18).map((glass, index) => (
+        
+        <SwiperSlide key={index}>
+            <GlassCard glass={glass} />
+            </SwiperSlide>
+
+        
+      ))
+      }
+
+      </Swiper>
+    </div>
+  )
+}
+
+export default Recommend
