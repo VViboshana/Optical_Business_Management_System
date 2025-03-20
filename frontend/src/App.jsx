d9d216830fd0ba1609187b983aa0aa8d47a5dd0f
linara03 <it23377608@my.sliit.lk>
2025-03-20 16:44:35 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,8 @@ const App = () => {
       <Routes>
         <Route path = '/' element={<Home />} />
         <Route path = '/doctors' element={<Doctors />} />
-        <Route path = '/doctors/:speciality' element={<Doctors />} />
+
+         
         <Route path = '/login' element={<Login />} />
         <Route path = '/my-profile' element={<MyProfile />} />
         <Route path = '/my-appointments' element={<MyAppointments />} />
