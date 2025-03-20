7c247f18d886d93d8e8f19836bbc01292b7da87b
Kumod De Silva <116749725+KumodN@users.noreply.github.com>
2025-03-20 10:02:09 +0530

    initialize backend, database and created glass model

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
+    //routes
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  }
+
+  main().then(()=>console.log("Mongodb Connected Successfully")).catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ backend/src/glasses/glass.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const glassSchema = new mongoose.Schema({
+    title: {
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    } ,
+    trending:{
+        type:Boolean,
+        required:true,
+    } ,
+    coverImage:{
+        type:String,
+        required:true,
+    } ,
+    oldPrice:Number,
+    newPrice:Number,
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    }
+  },{
+    timestamps:true
+  });
+
+  const Glass = mongoose.model('Glass',glassSchema);
+
+  module.export = Glass;
