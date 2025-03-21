ce9dc0c85b5efa2a74ba82c1b1b7c6c30cb847b6
Vihaga Viboshana <vihagaviboshana549@gmail.com>
2025-03-21 15:30:31 +0530

    Update mongodb.js

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
 
     mongoose.connection.on('connected', ()=> console.log("Database conected"));
 
-    await mongoose.connect(`${process.env.MONGODB_URL}/admin_dashbord`);
+    await mongoose.connect(`${process.env.MONGODB_URL}/optical-business-management`);
 };
 
 export default connectDB;
