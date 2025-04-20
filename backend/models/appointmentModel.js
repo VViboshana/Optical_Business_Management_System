48507cd33040ae00e2d85887500bbeb0322d4799
linara03 <it23377608@my.sliit.lk>
2025-04-20 20:04:14 +0530

    Update appointmentModel.js

--- backend/models/appointmentModel.js
+++ backend/models/appointmentModel.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose');
 
 const appointmentSchema = new mongoose.Schema({
-
   name: { type: String,required: true,},
   email: { type: String, required: true,},
   phone: {type: String,required: true,},
